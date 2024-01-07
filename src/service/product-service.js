383e9096beb9f9108fd690767159b924a24f79b1
JorgeMirandaM <jorge100099@hotmail.com>
2024-01-07 14:16:33 -0600

    Prueba con link de backend en producción

--- src/service/product-service.js
+++ src/service/product-service.js
@@ -1,6 +1,6 @@
 const listarProductos = async ()=>{
     try {
-        const respuesta = await fetch('http://localhost:4000/api/productos/getProducts');
+        const respuesta = await fetch('https://backend-alurageek-web.onrender.com/api/productos/getProducts');
         return await respuesta.json();
     } catch (error) {
         console.log(error);
@@ -12,7 +12,7 @@ const listarProductos = async ()=>{
 
 const crearProducto= async (data)=>{
     try {
-        const response= await fetch('http://localhost:4000/api/productos/createProduct',{
+        const response= await fetch('https://backend-alurageek-web.onrender.com/api/productos/createProduct',{
             method:'POST',
             body:data,
             credentials:'include'
@@ -27,7 +27,7 @@ const crearProducto= async (data)=>{
 
 const eliminarProducto= async (id)=>{
     try {
-         const response= await fetch(`http://localhost:4000/api/productos/deleteProductById/${id}`,{
+         const response= await fetch(`https://backend-alurageek-web.onrender.com/api/productos/deleteProductById/${id}`,{
             method:'DELETE',
             credentials:'include'
         })
@@ -40,7 +40,7 @@ const eliminarProducto= async (id)=>{
 
 const detalleProducto= async (id)=>{
     try {
-        const response= await fetch(`http://localhost:4000/api/productos/getProductById/${id}`,{
+        const response= await fetch(`https://backend-alurageek-web.onrender.com/api/productos/getProductById/${id}`,{
             method:'GET',
         });
         return await response.json();
@@ -52,7 +52,7 @@ const detalleProducto= async (id)=>{
 
 const obtenerProductoPorCategoria= async (categoria)=>{
     try {
-        const response= await fetch(`http://localhost:4000/api/productos/getProductsByCategory/${categoria}`);
+        const response= await fetch(`https://backend-alurageek-web.onrender.com/api/productos/getProductsByCategory/${categoria}`);
         return await response.json();
     } catch (error) {
         console.log(error);
@@ -63,7 +63,7 @@ const obtenerProductoPorCategoria= async (categoria)=>{
 const actualizarProducto= async (id,data)=>{
     
     try {
-        const response= await fetch(`http://localhost:4000/api/productos/updateProductById/${id}`,{
+        const response= await fetch(`https://backend-alurageek-web.onrender.com/api/productos/updateProductById/${id}`,{
             method:'PUT',
             body:data,
             credentials:'include'
--- src/service/user-service.js
+++ src/service/user-service.js
@@ -1,6 +1,6 @@
 const signin= async (email,password)=>{
     try {
-        const response= await fetch('http://localhost:4000/api/auth/signin',{
+        const response= await fetch('https://backend-alurageek-web.onrender.com/api/auth/signin',{
             method:'POST',
             body:JSON.stringify({email,password}),
             headers:{
@@ -17,7 +17,7 @@ const signin= async (email,password)=>{
 
 const signup= async (nombre,email,password)=>{
     try {
-        const response= await fetch('http://localhost:4000/api/auth/signup',{
+        const response= await fetch('https://backend-alurageek-web.onrender.com/api/auth/signup',{
             method:'POST',
             body:JSON.stringify({nombre,email,password}),
             headers:{'Content-Type':'application/json'},
@@ -33,7 +33,7 @@ const signup= async (nombre,email,password)=>{
 
 const logout= async ()=>{
     try {
-        const response=  await fetch('http://localhost:4000/api/auth/logout',{
+        const response=  await fetch('https://backend-alurageek-web.onrender.com/api/auth/logout',{
             method:'POST',
             credentials:'include'
         });
@@ -46,7 +46,7 @@ const logout= async ()=>{
 
 const getCuenta= async()=>{
     try {
-        const response=  await fetch('http://localhost:4000/api/auth/getCuenta',{
+        const response=  await fetch('https://backend-alurageek-web.onrender.com/api/auth/getCuenta',{
             credentials:'include'
         });
         return await response.json();
